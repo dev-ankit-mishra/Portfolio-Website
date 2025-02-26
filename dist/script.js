62d8d70c55a60268ed17dd65fea3724b32d3c3a7
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-02-26 18:40:41 +0530

    Added Click events to Projects

--- dist/script.js
+++ dist/script.js
@@ -8,6 +8,41 @@ resume.addEventListener("click",()=>{
 
 })
 
-//
+//Project Open Events
+
+const project1=document.querySelector("#project1");
+
+const project2=document.querySelector("#project2");
+
+const project3=document.querySelector("#project3");
+
+//Project 1-
+
+project1.addEventListener("click",()=>{
+
+    window.open("https://github.com/dev-ankit-mishra/Portfolio-Website","_blank");
+
+})
+
+//Project 2-
+
+project2.addEventListener("click",()=>{
+
+    window.open("https://github.com/dev-ankit-mishra/Portfolio-Website","_blank");
+
+})
+
+//Project 3-
+
+
+project3.addEventListener("click",()=>{
+
+    window.open("https://github.com/dev-ankit-mishra/Portfolio-Website","_blank");
+
+})
+
+
+
+
 
 
