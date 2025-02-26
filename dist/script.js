6ca07e6036bc8bc83a48ab5f4519018a3b93d8f2
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-02-26 18:23:37 +0530

    Added the click listener to download resume buttton

---
+++ dist/script.js
@@ -0,0 +1,13 @@
+//Download Resume Events
+
+const resume=document.querySelector("#resume")
+
+resume.addEventListener("click",()=>{
+
+    window.open("./documents/resume.pdf","_blank");
+
+})
+
+//
+
+
