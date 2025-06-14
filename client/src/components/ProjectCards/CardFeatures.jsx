3a4d824d0f9fa06c09669e7b2974b2f03f354907
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-06-14 23:10:24 +0530

    Added features of each card

---
+++ client/src/components/ProjectCards/CardFeatures.jsx
@@ -0,0 +1,15 @@
+export const codeEditorFeatures = [
+  "Real-time code sync via WebSockets",
+  "Supports multiple programming languages like JavaScript, Python, and C++.",
+  "Syntax highlighting for multiple langs",
+  "File tree sidebar and tabbed editor ",
+  "Integrated terminal (coming soon)",
+];
+
+export const portfolioFeatures = [
+  "Responsive design (mobile, tablet, desktop support)",
+  "Accessible design with keyboard support",
+  "Projects showcase with links to live demos & GitHub",
+  "Light/Dark mode toggle",
+  "Contact form and social media links",
+];
