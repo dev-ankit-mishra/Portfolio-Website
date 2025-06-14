ee71d698ed37b338c5254ae7a1be4f9ebb186889
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-06-14 23:10:49 +0530

    added the tech stack of each cards

---
+++ client/src/components/ProjectCards/CardTechStack.jsx
@@ -0,0 +1,37 @@
+export const portfolioTechItems = [
+  {
+    label: "React",
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg",
+  },
+  {
+    label: "JavaScript",
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg",
+  },
+  {
+    label: "Tailwind",
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg",
+  },
+];
+
+export const codeEditorTechItems = [
+  {
+    label: "React",
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg",
+  },
+  {
+    label: "Node.js",
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original.svg",
+  },
+  {
+    label: "Express",
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/express/express-original.svg",
+  },
+  {
+    label: "Socket.Io",
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/socketio/socketio-original.svg",
+  },
+  {
+    label: "TypeScript",
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg",
+  },
+];
