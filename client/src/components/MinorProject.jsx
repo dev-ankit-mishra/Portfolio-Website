59e00c64a7b1c4d5a16112cd3220dc4fe2247907
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-06-14 23:11:48 +0530

    Added the new minor project section

---
+++ client/src/components/MinorProject.jsx
@@ -0,0 +1,13 @@
+import { Section } from "lucide-react";
+
+export default function MinorProject() {
+  return (
+    <section className="h-screen w-full bg-neutral-800 tracking-wide leading-relaxed">
+      <header>
+        <h1 className="text-2xl text-center py-6 ">Minor Project</h1>
+        <div className="h-1 w-10 bg-blue-500 mt-2 mx-auto"></div>
+      </header>
+      <main></main>
+    </section>
+  );
+}
