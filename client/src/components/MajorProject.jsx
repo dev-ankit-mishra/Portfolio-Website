069767101060d64ada50701ff15587d538addaa9
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-06-15 23:06:53 +0530

    updated the major project section

--- client/src/components/MajorProject.jsx
+++ client/src/components/MajorProject.jsx
@@ -1,18 +1,18 @@
-import Cards from "./ProjectCards/Cards";
+import Cards from "./MajorProjectCard/Cards";
 
 export default function Project() {
   return (
     <section
-      className="scroll-mt-10 w-full h-screen bg-neutral-950"
+      className="scroll-mt-10 w-full h-screen bg-gradient-to-br from-[#302b63] to-[#2b0f54]"
       id="Projects"
     >
       <header>
-        <h1 className="text-3xl text-center font-semibold pt-10 pb-3 text-neutral-100">
+        <h1 className="text-3xl text-center font-semibold pt-4 pb-2 text-neutral-100">
           Major Project
         </h1>
-        <div className="w-24 h-1 bg-blue-500 rounded-2xl mx-auto mt-2"></div>
+        <div className="w-10 h-1 bg-blue-500 hover:w-24 transition-all duration-200 rounded-2xl mx-auto mt-2" />
       </header>
-      <main className="w-full h-full tracking-wide leading-relaxed flex flex-row justify-evenly items-center pb-35  ">
+      <main className="w-full h-full  flex flex-row justify-evenly items-center pb-30  ">
         <Cards />
       </main>
     </section>
