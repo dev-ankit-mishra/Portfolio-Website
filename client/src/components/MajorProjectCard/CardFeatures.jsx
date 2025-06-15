783fff45454a7c282c34b23cdf3db47494db04ac
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-06-15 23:04:34 +0530

    updated the card features

---
+++ client/src/components/MajorProjectCard/CardFeatures.jsx
@@ -0,0 +1,13 @@
+export const codeEditorFeatures = [
+  "Real-time code sync via WebSockets",
+  "Syntax highlighting for multiple langs",
+  "File tree sidebar and tabbed editor ",
+  "Integrated terminal (coming soon)",
+];
+
+export const portfolioFeatures = [
+  "Responsive design (mobile/tablet/desktop)",
+  "Accessible design with keyboard support",
+  "Projects showcase with links to live demos & GitHub",
+  "Light/Dark mode toggle",
+];
