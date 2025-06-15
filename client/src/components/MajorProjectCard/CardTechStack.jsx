77daff234cd5dd48a16b38194a41ea1c4b518af8
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-06-15 23:03:31 +0530

    updated card tech stack

---
+++ client/src/components/MajorProjectCard/CardTechStack.jsx
@@ -0,0 +1,33 @@
+export const portfolioTechItems = [
+  {
+    label: "React",
+    bgClass: "bg-gray-700",
+  },
+  {
+    label: "JavaScript",
+    bgClass: "bg-yellow-700",
+  },
+  {
+    label: "Tailwind",
+    bgClass: "bg-cyan-700",
+  },
+];
+
+export const codeEditorTechItems = [
+  {
+    label: "React",
+    bgClass: "bg-gray-700",
+  },
+  {
+    label: "Express",
+    bgClass: "bg-emerald-700",
+  },
+  {
+    label: "Socket.Io",
+    bgClass: "bg-rose-700",
+  },
+  {
+    label: "TypeScript",
+    bgClass: "bg-blue-700",
+  },
+];
