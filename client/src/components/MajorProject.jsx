5e89361ca3d76272ab46c1519ed4ecd7f069487b
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-06-17 21:53:35 +0530

    updated the width and spacing of project cards

--- client/src/components/MajorProject.jsx
+++ client/src/components/MajorProject.jsx
@@ -7,12 +7,12 @@ export default function Project() {
       id="Projects"
     >
       <header>
-        <h1 className="text-3xl text-center font-semibold pt-4 pb-2 text-neutral-100">
+        <h1 className="text-3xl text-center font-semibold pt-8 pb-2 text-neutral-100">
           Major Project
         </h1>
         <div className="w-10 h-1 bg-blue-500 hover:w-24 transition-all duration-200 rounded-2xl mx-auto mt-2" />
       </header>
-      <main className="w-full h-full  flex flex-row justify-evenly items-center pb-30  ">
+      <main className="w-full h-full  flex flex-row justify-evenly items-center pb-24  ">
         <Cards />
       </main>
     </section>
