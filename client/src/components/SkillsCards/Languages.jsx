08eb04a59df21f114119bbc1127df559ac28a944
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-06-17 21:51:17 +0530

    updatede the bg-color

--- client/src/components/SkillsCards/Languages.jsx
+++ client/src/components/SkillsCards/Languages.jsx
@@ -1,57 +1,56 @@
 import { motion } from "framer-motion";
 import Tilt from "react-parallax-tilt";
 
+import {
+  SiC,
+  SiCplusplus,
+  SiTypescript,
+  SiJavascript,
+  SiPython,
+} from "react-icons/si";
+import { FaJava } from "react-icons/fa";
+
 const languagesItems = [
   {
     id: 1,
-    src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/c/c-original.svg",
+    icon: <SiC className="text-3xl text-blue-500" />,
     label: "C",
   },
   {
     id: 2,
-    src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/java/java-original.svg",
+    icon: <FaJava className="text-3xl text-red-600" />,
     label: "Java",
   },
   {
     id: 3,
-    src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg",
+    icon: <SiTypescript className="text-3xl text-blue-400" />,
     label: "TypeScript",
   },
   {
     id: 4,
-    src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg",
+    icon: <SiJavascript className="text-3xl text-yellow-400" />,
     label: "JavaScript",
   },
   {
     id: 5,
-    src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/cplusplus/cplusplus-original.svg",
+    icon: <SiCplusplus className="text-3xl text-indigo-400" />,
     label: "C++",
   },
   {
     id: 6,
-    src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg",
+    icon: <SiPython className="text-3xl text-yellow-300" />,
     label: "Python",
   },
 ];
 
 export const languages = languagesItems.map((lang) => {
   return (
-    <motion.div
-      initial={{ opacity: 0, scale: 0.9 }}
-      whileInView={{ opacity: 1, scale: 1 }}
-      transition={{ duration: 0.5 }}
-    >
+    <div>
       {/* single skill */}
-      <div className="flex flex-col items-center">
-        <img
-          className="w-8 h-8 hover:scale-110 transition-transform duration-300 hover:shadow-lg hover:shadow-blue-400/30 rounded-md"
-          src={lang.src}
-          alt={lang.label + "-icon"}
-        />
-        <span className="text-sm tracking-wide text-neutral-100 mt-1">
-          {lang.label}
-        </span>
+      <div className="flex flex-col items-center hover:scale-105 hover:shadow-md transition-all duration-200">
+        {lang.icon}
+        <span className="text-sm tracking-wide mt-1">{lang.label}</span>
       </div>
-    </motion.div>
+    </div>
   );
 });
