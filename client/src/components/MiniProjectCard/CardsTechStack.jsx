4e05b9aed59d8b30b71b04d72f45a4754989721b
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-06-18 13:06:36 +0530

    Updated the card tech stack of minor projects

--- client/src/components/MiniProjectCard/CardsTechStack.jsx
+++ client/src/components/MiniProjectCard/CardsTechStack.jsx
@@ -1,1 +1,74 @@
-const assemblyGame = [];
+export const assemblyGame = [
+  {
+    id: 1,
+    label: "React",
+    bgClass: "bg-blue-900/20 text-blue-300",
+  },
+  {
+    id: 2,
+    label: "JavaScript",
+    bgClass: "bg-gray-700 text-white",
+  },
+  {
+    id: 3,
+    label: "CSS",
+    bgClass: "bg-yellow-400 text-black",
+  },
+];
+
+export const tenzieGame = [
+  {
+    id: 1,
+    label: "React",
+    bgClass: "bg-blue-900/20 text-blue-300",
+  },
+  {
+    id: 2,
+    label: "Vite",
+    bgClass: "bg-pink-700 text-white",
+  },
+  {
+    id: 3,
+    label: "CSS",
+    bgClass: "bg-yellow-400 text-black",
+  },
+];
+export const deckGame = [
+  {
+    id: 1,
+    label: "JavaScript",
+    bgClass: "bg-gray-700 text-white",
+  },
+  {
+    id: 2,
+    label: "HTML",
+    bgClass: "bg-orange-600 text-white",
+  },
+  {
+    id: 3,
+    label: "CSS",
+    bgClass: "bg-yellow-400 text-black",
+  },
+];
+export const movieWatchlist = [
+  {
+    id: 1,
+    label: "HTML",
+    bgClass: "bg-orange-600 text-white",
+  },
+  {
+    id: 2,
+    label: "CSS",
+    bgClass: "bg-yellow-400 text-black",
+  },
+  {
+    id: 3,
+    label: "JavaScript",
+    bgClass: "bg-gray-700 text-white",
+  },
+  {
+    id: 4,
+    label: "OMDb API",
+    bgClass: "bg-green-700 text-white",
+  },
+];
