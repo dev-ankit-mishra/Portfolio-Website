54eae9edbef243e4730f433ae92a3c7dd8758e30
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-06-18 23:40:31 +0530

    Updated the major project bg-color

--- client/src/components/MajorProject.jsx
+++ client/src/components/MajorProject.jsx
@@ -3,11 +3,17 @@ import Cards from "./MajorProjectCard/Cards";
 export default function Project() {
   return (
     <section
-      className="scroll-mt-10 w-full h-screen bg-gradient-to-br from-[#302b63] to-[#2b0f54]"
+      className=" w-full h-screen bg-gradient-to-br from-[#1e1b4b] via-[#312e81] to-[#1e1b4b]
+
+
+
+
+
+"
       id="Projects"
     >
       <header>
-        <h1 className="text-3xl text-center font-semibold pt-8 pb-2 text-neutral-100">
+        <h1 className="text-3xl text-center font-bold pt-8 pb-2 text-neutral-100">
           Major Project
         </h1>
         <div className="w-10 h-1 bg-blue-500 hover:w-24 transition-all duration-200 rounded-2xl mx-auto mt-2" />
