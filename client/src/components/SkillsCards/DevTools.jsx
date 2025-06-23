4782b0fa862df05ecffc5f95b1161bdddacc2270
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-06-23 20:14:20 +0530

    added tool tip to devtools

--- client/src/components/SkillsCards/DevTools.jsx
+++ client/src/components/SkillsCards/DevTools.jsx
@@ -1,31 +1,37 @@
 import { motion } from "framer-motion";
 import { SiGit, SiGithub, SiFigma, SiPostman, SiVite } from "react-icons/si";
+import ToolTip from "../ToolTip";
 
 const DevToolsItems = [
   {
     id: 1,
     icon: <SiGit className="text-3xl text-red-500" />,
     label: "Git",
+    tooltip: "Version control system to track code changes",
   },
   {
     id: 2,
     icon: <SiGithub className="text-3xl text-white" />,
     label: "Github",
+    tooltip: "Code hosting platform for collaboration",
   },
   {
     id: 3,
     icon: <SiFigma className="text-3xl text-pink-500" />,
     label: "Figma",
+    tooltip: "Design and prototyping tool",
   },
   {
     id: 4,
     icon: <SiPostman className="text-3xl text-orange-500" />,
     label: "Postman",
+    tooltip: "API testing and documentation tool",
   },
   {
     id: 5,
     icon: <SiVite className="text-3xl text-purple-400" />,
     label: "Vite",
+    tooltip: "Fast frontend build tool",
   },
 ];
 
@@ -33,10 +39,12 @@ export const devTools = DevToolsItems.map((tool) => {
   return (
     <div>
       {/* single skill */}
-      <div className="flex flex-col items-center hover:scale-105 hover:shadow-md transition-all duration-200">
-        {tool.icon}
-        <span className="text-sm tracking-wide  mt-1">{tool.label}</span>
-      </div>
+      <ToolTip text={tool.tooltip}>
+        <div className="flex flex-col items-center hover:scale-105 hover:shadow-md transition-all duration-200">
+          {tool.icon}
+          <span className="text-sm tracking-wide  mt-1">{tool.label}</span>
+        </div>
+      </ToolTip>
     </div>
   );
 });
