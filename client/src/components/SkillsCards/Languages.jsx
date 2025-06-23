f6d2f3d7d6e7c4e3da50f3d71df72fa7dadf85a7
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-06-23 20:14:36 +0530

    Added tool tip to language

--- client/src/components/SkillsCards/Languages.jsx
+++ client/src/components/SkillsCards/Languages.jsx
@@ -9,37 +9,44 @@ import {
   SiPython,
 } from "react-icons/si";
 import { FaJava } from "react-icons/fa";
+import ToolTip from "../ToolTip";
 
 const languagesItems = [
   {
     id: 1,
     icon: <SiC className="text-3xl text-blue-500" />,
     label: "C",
+    tooltip: "Low-level system programming language",
   },
   {
     id: 2,
     icon: <FaJava className="text-3xl text-red-600" />,
     label: "Java",
+    tooltip: "Object-oriented programming for cross-platform apps",
   },
   {
     id: 3,
     icon: <SiTypescript className="text-3xl text-blue-400" />,
     label: "TypeScript",
+    tooltip: "Typed superset of JavaScript",
   },
   {
     id: 4,
     icon: <SiJavascript className="text-3xl text-yellow-400" />,
     label: "JavaScript",
+    tooltip: "The language of the web",
   },
   {
     id: 5,
     icon: <SiCplusplus className="text-3xl text-indigo-400" />,
     label: "C++",
+    tooltip: "Powerful language for performance-critical applications",
   },
   {
     id: 6,
     icon: <SiPython className="text-3xl text-yellow-300" />,
     label: "Python",
+    tooltip: "High-level language for AI, automation, and more",
   },
 ];
 
@@ -47,10 +54,12 @@ export const languages = languagesItems.map((lang) => {
   return (
     <div>
       {/* single skill */}
-      <div className="flex flex-col items-center hover:scale-105 hover:shadow-md transition-all duration-200">
-        {lang.icon}
-        <span className="text-sm tracking-wide mt-1">{lang.label}</span>
-      </div>
+      <ToolTip text={lang.tooltip}>
+        <div className="flex flex-col items-center hover:scale-105 hover:shadow-md transition-all duration-200">
+          {lang.icon}
+          <span className="text-sm tracking-wide mt-1">{lang.label}</span>
+        </div>
+      </ToolTip>
     </div>
   );
 });
