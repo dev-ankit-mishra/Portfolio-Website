b3b36863e69c6845fb8b3305981366cf865138b5
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-06-23 20:13:09 +0530

    Added tooltip ai card

--- client/src/components/SkillsCards/AITools.jsx
+++ client/src/components/SkillsCards/AITools.jsx
@@ -1,5 +1,6 @@
 import { motion } from "framer-motion";
 import Tilt from "react-parallax-tilt";
+import ToolTip from "../ToolTip";
 
 import { SiTensorflow } from "react-icons/si";
 
@@ -8,6 +9,7 @@ const AIToolsItems = [
     id: 1,
     icon: <SiTensorflow className="text-3xl text-orange-500" />,
     label: "TensorFlow",
+    tooltip: "Open-source library for machine learning",
   },
 ];
 
@@ -15,10 +17,12 @@ export const aiTools = AIToolsItems.map((tool) => {
   return (
     <div>
       {/* single skill */}
-      <div className="flex flex-col items-center hover:scale-105 hover:shadow-md transition-all duration-200">
-        {tool.icon}
-        <span className="text-sm tracking-wide  mt-1">{tool.label}</span>
-      </div>
+      <ToolTip text={tool.tooltip}>
+        <div className="flex flex-col items-center hover:scale-105 hover:shadow-md transition-all duration-200">
+          {tool.icon}
+          <span className="text-sm tracking-wide  mt-1">{tool.label}</span>
+        </div>
+      </ToolTip>
     </div>
   );
 });
