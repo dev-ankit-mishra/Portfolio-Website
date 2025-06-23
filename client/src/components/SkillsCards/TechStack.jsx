db233ae9bb73ab3139134373253d52e09a035a51
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-06-23 20:14:50 +0530

    added tool tip to tech stack

--- client/src/components/SkillsCards/TechStack.jsx
+++ client/src/components/SkillsCards/TechStack.jsx
@@ -1,5 +1,6 @@
 import { motion } from "framer-motion";
 import Tilt from "react-parallax-tilt";
+import ToolTip from "../ToolTip";
 
 import {
   SiHtml5,
@@ -15,31 +16,37 @@ const techStackItems = [
     id: 1,
     icon: <SiHtml5 className="text-3xl text-orange-500" />,
     label: "HTML",
+    tooltip: "Structure the content of web pages",
   },
   {
     id: 2,
     icon: <SiTailwindcss className="text-3xl text-cyan-400" />,
     label: "Tailwind",
+    tooltip: "Utility-first CSS framework for rapid UI development",
   },
   {
     id: 3,
     icon: <SiReact className="text-3xl text-sky-400" />,
     label: "React",
+    tooltip: "Build dynamic user interfaces with components",
   },
   {
     id: 4,
     icon: <SiNodedotjs className="text-3xl text-green-500" />,
     label: "Node",
+    tooltip: "JavaScript runtime for backend development",
   },
   {
     id: 5,
     icon: <SiExpress className="text-3xl text-gray-300" />,
     label: "Express",
+    tooltip: "Minimalist Node.js framework for APIs",
   },
   {
     id: 6,
     icon: <SiNextdotjs className="text-3xl text-white" />,
     label: "Next",
+    tooltip: "React framework for SSR and static websites",
   },
 ];
 
@@ -47,10 +54,12 @@ export const techStacks = techStackItems.map((lang) => {
   return (
     <div>
       {/* single skill */}
-      <div className="flex flex-col items-center hover:scale-105 hover:shadow-md transition-all duration-200">
-        {lang.icon}
-        <span className="text-sm tracking-wide  mt-1">{lang.label}</span>
-      </div>
+      <ToolTip text={lang.tooltip}>
+        <div className="flex flex-col items-center hover:scale-105 hover:shadow-md transition-all duration-200">
+          {lang.icon}
+          <span className="text-sm tracking-wide  mt-1">{lang.label}</span>
+        </div>
+      </ToolTip>
     </div>
   );
 });
