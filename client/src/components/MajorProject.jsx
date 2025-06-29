5b88b2fac730b0e79bc43679a34fb60a9a21dc7b
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-06-29 22:25:21 +0530

    Added some padding

--- client/src/components/MajorProject.jsx
+++ client/src/components/MajorProject.jsx
@@ -3,7 +3,7 @@ import Cards from "./MajorProjectCard/Cards";
 export default function Project() {
   return (
     <section
-      className=" w-full h-auto bg-gradient-to-br from-[#1e1b4b] via-[#312e81] to-[#1e1b4b]
+      className=" w-full bg-gradient-to-br py-6 from-[#1e1b4b] via-[#312e81] to-[#1e1b4b]
 
 
 
@@ -13,12 +13,12 @@ export default function Project() {
       id="Projects"
     >
       <header>
-        <h1 className="text-3xl text-center font-bold pt-8 pb-2 text-neutral-100">
+        <h1 className="text-2xl md:text-3xl text-center font-bold pt-6 pb-2 text-neutral-100">
           Major Project
         </h1>
-        <div className="w-10 h-1 bg-blue-500 hover:w-24 transition-all duration-200 rounded-2xl mx-auto mt-2" />
+        <div className="w-10 h-1 bg-blue-500 hover:w-24 transition-all duration-200 rounded-2xl mx-auto mt-2 mb-4" />
       </header>
-      <main className="w-full h-full  flex flex-col  gap-10  md:flex-row md:justify-evenly px-4 items-center py-10 lg:pb-24  ">
+      <main className="w-full h-full  flex flex-col  gap-10  md:flex-row md:justify-evenly px-4 items-center py-4 lg:pb-24  ">
         <Cards />
       </main>
     </section>
