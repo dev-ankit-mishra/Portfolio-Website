2ac3db33ee923028b225240f4800335da647428e
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-06-29 11:52:56 +0530

    made it rseponsive for sm and md screen

--- client/src/components/ToolTip.jsx
+++ client/src/components/ToolTip.jsx
@@ -2,7 +2,7 @@ export default function ToolTip({ children, text }) {
   return (
     <div className="relative group inline-block">
       {children}
-      <div className="absolute bottom-full mb-2 hidden group-hover:block bg-gray-800 text-white text-xs rounded px-2 py-1 whitespace-nowrap z-10">
+      <div className="absolute bottom-full mb-2 hidden group-hover:block bg-gray-800 text-white text-[0.6rem] md:text-xs rounded px-2 py-1 whitespace-nowrap z-10">
         {text}
       </div>
     </div>
