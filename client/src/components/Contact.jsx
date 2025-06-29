755a83170eac6769da2bb0725355ac2b82c84e19
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-06-29 18:38:23 +0530

    added the pading for 2xl screens

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -7,7 +7,7 @@ export default function Contact() {
       className=" w-full h-fit xl:h-screen  flex flex-col  gap-4   text-neutral-100 bg-gradient-to-br from-[#000000] to-[#141e30]"
       id="Contact"
     >
-      <main className="flex-1 xl:px-60 md:px-24 lg:px-36 px-10 space-y-4 pb-4 xl:pb-0 lg:pt-4 xl:pt-8">
+      <main className="flex-1 2xl:px-70 xl:px-60 md:px-24 lg:px-36 px-10 space-y-4 pb-4 xl:pb-0 lg:pt-4 xl:pt-8">
         <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold mt-12">
           Let's Connect
         </h1>
