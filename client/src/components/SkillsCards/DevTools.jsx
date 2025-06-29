32f4b25c0382c024b5b01f7d7d21a4fbfed7ce35
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-06-29 11:53:25 +0530

    made about page rseponsive for sm and md screen

--- client/src/components/SkillsCards/DevTools.jsx
+++ client/src/components/SkillsCards/DevTools.jsx
@@ -5,31 +5,31 @@ import ToolTip from "../ToolTip";
 const DevToolsItems = [
   {
     id: 1,
-    icon: <SiGit className="text-3xl text-red-500" />,
+    icon: <SiGit className="text-2xl lg:text-3xl text-red-500" />,
     label: "Git",
     tooltip: "Version control system to track code changes",
   },
   {
     id: 2,
-    icon: <SiGithub className="text-3xl text-white" />,
+    icon: <SiGithub className="text-2xl lg:text-3xl text-white" />,
     label: "Github",
     tooltip: "Code hosting platform for collaboration",
   },
   {
     id: 3,
-    icon: <SiFigma className="text-3xl text-pink-500" />,
+    icon: <SiFigma className="text-2xl lg:text-3xl text-pink-500" />,
     label: "Figma",
     tooltip: "Design and prototyping tool",
   },
   {
     id: 4,
-    icon: <SiPostman className="text-3xl text-orange-500" />,
+    icon: <SiPostman className="text-2xl lg:text-3xl text-orange-500" />,
     label: "Postman",
     tooltip: "API testing and documentation tool",
   },
   {
     id: 5,
-    icon: <SiVite className="text-3xl text-purple-400" />,
+    icon: <SiVite className="text-2xl lg:text-3xl text-purple-400" />,
     label: "Vite",
     tooltip: "Fast frontend build tool",
   },
@@ -42,7 +42,9 @@ export const devTools = DevToolsItems.map((tool) => {
       <ToolTip text={tool.tooltip}>
         <div className="flex flex-col items-center hover:scale-105 hover:shadow-md transition-all duration-200">
           {tool.icon}
-          <span className="text-sm tracking-wide  mt-1">{tool.label}</span>
+          <span className="lg:text-sm text-xs tracking-wide  mt-1">
+            {tool.label}
+          </span>
         </div>
       </ToolTip>
     </div>
