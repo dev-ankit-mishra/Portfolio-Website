67d022049e3e4dd004b38b3a1f7c879ff4e9ed6a
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-06-29 11:53:19 +0530

    made about page rseponsive for sm and md screen

--- client/src/components/SkillsCards/Databases.jsx
+++ client/src/components/SkillsCards/Databases.jsx
@@ -7,25 +7,25 @@ import { SiMysql, SiMongodb, SiPostgresql, SiFirebase } from "react-icons/si";
 const DatabaseItems = [
   {
     id: 1,
-    icon: <SiMongodb className="text-3xl text-green-600" />,
+    icon: <SiMongodb className="text-2xl lg:text-3xl text-green-600" />,
     label: "MongoDB",
     tooltip: "NoSQL database for flexible document storage",
   },
   {
     id: 2,
-    icon: <SiPostgresql className="text-3xl text-sky-600" />,
+    icon: <SiPostgresql className="text-2xl lg:text-3xl text-sky-600" />,
     label: "PostgreSQL",
     tooltip: "Powerful open-source relational database",
   },
   {
     id: 3,
-    icon: <SiMysql className="text-3xl text-blue-400" />,
+    icon: <SiMysql className="text-2xl lg:text-3xl text-blue-400" />,
     label: "MySQL",
     tooltip: "Popular relational database for web apps",
   },
   {
     id: 4,
-    icon: <SiFirebase className="text-3xl text-yellow-400" />,
+    icon: <SiFirebase className="text-2xl lg:text-3xl text-yellow-400" />,
     label: "Firebase",
     tooltip: "Google's real-time database and hosting platform",
   },
@@ -38,7 +38,9 @@ export const databases = DatabaseItems.map((database) => {
       <ToolTip text={database.tooltip}>
         <div className="flex flex-col items-center hover:scale-105 hover:shadow-md transition-all duration-200">
           {database.icon}
-          <span className="text-sm tracking-wide  mt-1">{database.label}</span>
+          <span className="lg:text-sm text-xs tracking-wide  mt-1">
+            {database.label}
+          </span>
         </div>
       </ToolTip>
     </div>
