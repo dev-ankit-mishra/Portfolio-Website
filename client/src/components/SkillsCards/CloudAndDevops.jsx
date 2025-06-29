7d03c04b5aecd44fc30654450965aa5ae5b3cf98
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-06-29 11:53:10 +0530

    made about page rseponsive for sm and md screen

--- client/src/components/SkillsCards/CloudAndDevops.jsx
+++ client/src/components/SkillsCards/CloudAndDevops.jsx
@@ -6,19 +6,19 @@ import { SiNetlify, SiVercel, SiDocker } from "react-icons/si";
 const cloudAndDevopsItems = [
   {
     id: 1,
-    icon: <SiNetlify className="text-3xl text-cyan-400" />,
+    icon: <SiNetlify className="text-2xl lg:text-3xl text-cyan-400" />,
     label: "Netlify",
     tooltip: "Deploy static websites easily",
   },
   {
     id: 2,
-    icon: <SiVercel className="text-3xl text-white" />,
+    icon: <SiVercel className="text-2xl lg:text-3xl text-white" />,
     label: "Vercel",
     tooltip: "Frontend platform for React and Next.js apps",
   },
   {
     id: 3,
-    icon: <SiDocker className="text-3xl text-blue-400" />,
+    icon: <SiDocker className="text-2xl lg:text-3xl text-blue-400" />,
     label: "Docker",
     tooltip: "Containerization for consistent app environments",
   },
@@ -31,7 +31,9 @@ export const cloudAndDevops = cloudAndDevopsItems.map((item) => {
       <ToolTip text={item.tooltip}>
         <div className="flex flex-col items-center hover:scale-105 hover:shadow-md transition-all duration-200">
           {item.icon}
-          <span className="text-sm tracking-wide  mt-1">{item.label}</span>
+          <span className="lg:text-sm text-xs tracking-wide  mt-1">
+            {item.label}
+          </span>
         </div>
       </ToolTip>
     </div>
