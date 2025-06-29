77c6db4286f6a1d15d12443d6589f1b75f8b0a3f
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-06-29 17:31:07 +0530

    made footer responsive

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,15 +2,15 @@ import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 export default function Footer() {
   return (
-    <footer className="w-full h-12 px-12 gap-6 text-lg tracking-wide  flex items-center justify-between absolute bottom-0 left-0 bg-neutral-950/90 backdrop-blur-md text-neutral-400 border-t border-neutral-800 z-50 ">
-      <p className="text-sm">© 2025 Ankit Mishra Development.</p>
+    <footer className="w-full h-12 px-4   gap-6  tracking-wide  flex items-center justify-between  bg-neutral-950/90  text-neutral-400 border-t border-white/10 z-50 ">
+      <p className="text-xs md:text-sm">© 2025 Ankit Mishra Development.</p>
       <a
         href="mailto:contact@ankitmishra.pro"
-        className="text-sm hover:text-blue-400 transition-colors"
+        className="text-sm hidden md:block hover:text-blue-400 transition-colors"
       >
         contact@ankitmishra.pro
       </a>
-      <div className="flex justify-center items-center gap-6">
+      <div className="flex justify-center items-center gap-4">
         <a href="https://github.com/dev-ankit-mishra" target="_blank">
           <FaGithub size={24} className="hover:text-gray-600 cursor-pointer" />
         </a>
