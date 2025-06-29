8173f5b7d97038c1e64ad19d2646ae0bd3784071
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-06-29 19:38:51 +0530

    Added swiper carousel

--- client/src/components/MinorProject.jsx
+++ client/src/components/MinorProject.jsx
@@ -1,35 +1,58 @@
-import { cards } from "./MiniProjectCard/Cards";
 import React from "react";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import { cards } from "./MiniProjectCard/Cards";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination, Navigation, Keyboard } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 
 export default function MinorProject() {
-  const settings = {
-    dots: false,
-    infinite: true,
-    centerPadding: "40px",
-    speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 3,
-  };
   return (
-    <section className="min-h-screen min-w-full bg-gradient-to-br from-[#0d0221] to-[#232526] text-white tracking-wide leading-relaxed">
+    <section className="w-full bg-gradient-to-br from-[#0d0221] to-[#232526] text-white tracking-wide leading-relaxed">
       <header>
-        <h1 className="text-3xl text-center font-bold py-3 pt-6 ">
+        <h1 className="text-3xl text-center font-bold py-3 pt-6">
           Mini Project
         </h1>
         <div className="w-10 h-1 bg-blue-500 hover:w-24 transition-all duration-200 rounded-2xl mx-auto mt-2" />
       </header>
       <main className="w-full h-full">
-        <div className="slider-container w-[80rem]  p-4 mx-auto ">
-          <Slider {...settings}>
+        <div className="slider-container max-w-[80rem]  py-6 mx-auto">
+          <Swiper
+            slidesPerView={2}
+            spaceBetween={30}
+            keyboard={{ enabled: true, onlyInViewport: true, pageUpDown: true }}
+            pagination={{ clickable: true }}
+            navigation={true}
+            autoplay={{
+              delay: 3000,
+              disableOnInteraction: false,
+            }}
+            modules={[Autoplay, Pagination, Navigation, Keyboard]}
+            breakpoints={{
+              0: {
+                slidesPerView: 1,
+              },
+              640: {
+                slidesPerView: 1,
+              },
+              1024: {
+                slidesPerView: 2,
+              },
+              1280: {
+                slidesPerView: 3, // Tailwind 'xl'
+              },
+            }}
+          >
             {cards.map((card, index) => (
-              <div key={index} className="py-4 px-5">
-                {card}
-              </div>
+              <SwiperSlide key={index}>
+                <div className="flex items-center justify-center py-4 px-5">
+                  <div className="max-w-md w-full">{card}</div>
+                </div>
+              </SwiperSlide>
             ))}
-          </Slider>
+          </Swiper>
         </div>
       </main>
     </section>
