d9b2da97c0429a76a516107f64142a4cc1e6cd29
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-06-29 17:30:13 +0530

    made contact responsive for smaller screens

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -4,16 +4,20 @@ import ContactForm from "./ContactForm";
 export default function Contact() {
   return (
     <section
-      className=" w-full h-screen relative flex flex-col px-100 py-4 gap-4   text-neutral-100 bg-gradient-to-br from-[#000000] to-[#141e30]"
+      className=" w-full h-fit xl:h-screen  flex flex-col  gap-4   text-neutral-100 bg-gradient-to-br from-[#000000] to-[#141e30]"
       id="Contact"
     >
-      <h1 className="text-4xl font-bold mt-12">Let's Connect</h1>
-      <p className="tracking-wide leading-relaxed text-neutral-200">
-        I’m always open to discussing new projects, creative ideas, or
-        opportunities to be part of your vision. Whether you have a question or
-        just want to say hi — I’ll try my best to get back to you!
-      </p>
-      <ContactForm />
+      <main className="flex-1 xl:px-60 md:px-24 lg:px-36 px-10 space-y-4 pb-4 xl:pb-0 lg:pt-4 xl:pt-8">
+        <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold mt-12">
+          Let's Connect
+        </h1>
+        <p className="tracking-wide text-xs md:text-sm lg:text-base leading-relaxed text-neutral-200">
+          I’m always open to discussing new projects, creative ideas, or
+          opportunities to be part of your vision. Whether you have a question
+          or just want to say hi — I’ll try my best to get back to you!
+        </p>
+        <ContactForm />
+      </main>
 
       <Footer />
     </section>
