df28557bf8142790ba42f169dbc7d4eb4e6f93f7
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-06-30 11:08:18 +0530

    made it responsive for xs screens

--- client/src/components/Skills.jsx
+++ client/src/components/Skills.jsx
@@ -34,6 +34,7 @@ export default function Skills() {
   const bgColors = {
     Languages: "bg-gradient-to-br from-[#0a1a3d] to-[#0f2c63]",
     "Tech Stacks": "bg-gradient-to-br from-[#0d1117] to-[#1f2937]",
+
     Databases: "bg-gradient-to-br from-[#002d2d] to-[#014f40]",
     "Cloud & Devops": "bg-gradient-to-br from-[#002d3d] to-[#004f73]",
     "AI Tools": "bg-gradient-to-br from-[#3a0d00] to-[#5a2000]",
@@ -42,7 +43,7 @@ export default function Skills() {
 
   const renderCard = (card) => (
     <div
-      className={`w-xs  xl:w-sm  rounded-2xl  
+      className={`w-[18rem] sm:w-xs  xl:w-sm  rounded-2xl  
                     shadow-xl shadow-black/40 transition-all duration-200 
                     hover:ring-1 hover:ring-cyan-400
                     hover:scale-102 hover:shadow-cyan-500/20 border border-white/10
@@ -69,7 +70,7 @@ export default function Skills() {
 
   return (
     <section
-      className="scroll-mt-8 w-full px-4 py-6 md:py-10 box-border bg-gradient-to-br from-[#1e293b] to-[#0f172a] pb-12"
+      className="scroll-mt-8 w-full  md:h-auto px-4 py-6 md:py-10 box-border bg-gradient-to-br from-[#1e293b] to-[#0f172a] pb-12"
       id="Skills"
     >
       <header>
@@ -79,28 +80,30 @@ export default function Skills() {
         <div className="w-10 h-1 bg-blue-500 hover:w-24 transition-all duration-200 rounded-2xl mx-auto mt-2 mb-8" />
       </header>
 
-      <main className="text-white w-full mx-auto px-4">
-        {isMobile ? (
-          <Swiper
-            effect="cards"
-            grabCursor={true}
-            loop={true}
-            autoplay={{
-              delay: 3000,
-              disableOnInteraction: false,
-            }}
-            modules={[EffectCards, Autoplay]}
-            className="mySwiper max-w-sm mx-auto"
-          >
-            {cardItems.map((card) => (
-              <SwiperSlide key={card.id}>{renderCard(card)}</SwiperSlide>
-            ))}
-          </Swiper>
-        ) : (
-          <div className="grid sm:grid-cols-2 [@media(min-width:1080px)]:grid-cols-3 gap-10 place-items-center">
-            {cardItems.map((card) => renderCard(card))}
-          </div>
-        )}
+      <main className="text-white h-full py-28 px-4">
+        <div className="">
+          {isMobile ? (
+            <Swiper
+              effect="cards"
+              grabCursor={true}
+              loop={true}
+              autoplay={{
+                delay: 4000,
+                disableOnInteraction: false,
+              }}
+              modules={[EffectCards, Autoplay]}
+              className="mySwiper max-w-sm mx-auto"
+            >
+              {cardItems.map((card) => (
+                <SwiperSlide key={card.id}>{renderCard(card)}</SwiperSlide>
+              ))}
+            </Swiper>
+          ) : (
+            <div className="grid sm:grid-cols-2 [@media(min-width:1080px)]:grid-cols-3 gap-10 place-items-center">
+              {cardItems.map((card) => renderCard(card))}
+            </div>
+          )}
+        </div>
       </main>
     </section>
   );
