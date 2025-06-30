05f4a888432f8eed7f78925a0a16e7bbd56280c2
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-07-01 00:11:46 +0530

    added light mode background and text color

--- client/src/components/ContactForm.jsx
+++ client/src/components/ContactForm.jsx
@@ -39,7 +39,6 @@ const ContactForm = () => {
       });
   };
 
-  // Determine button classes based on state
   const getButtonColor = () => {
     switch (buttonState) {
       case "processing":
@@ -59,36 +58,38 @@ const ContactForm = () => {
       onSubmit={sendEmail}
       className="flex flex-col space-y-3 mt-6"
     >
-      <label className="text-sm md:text-base lg:text-lg">Full name</label>
+      <label className="text-sm md:text-base lg:text-lg text-gray-700 dark:text-white">
+        Full name
+      </label>
       <input
         name="name"
         required
         autoComplete="off"
-        className="bg-neutral-800 p-2 rounded border text-white border-neutral-700 text-sm md:text-base lg:text-lg placeholder:text-neutral-400 focus:outline-none focus:ring-2 focus:ring-blue-600"
+        className="bg-white dark:bg-neutral-800 p-2 rounded border border-gray-300 dark:border-neutral-700 text-gray-800 dark:text-white text-sm md:text-base lg:text-lg placeholder:text-gray-400 dark:placeholder:text-neutral-400 focus:outline-none focus:ring-2 focus:ring-blue-600"
         type="text"
         placeholder="e.g., Ankit Mishra"
       />
 
-      <label className="text-sm md:text-base lg:text-lg mt-3">
+      <label className="text-sm md:text-base lg:text-lg mt-3 text-gray-700 dark:text-white">
         Email address
       </label>
       <input
         name="email"
         required
         autoComplete="off"
-        className="bg-neutral-800 p-2 rounded text-white border border-neutral-700 text-sm md:text-base lg:text-lg placeholder:text-neutral-400 focus:outline-none focus:ring-2 focus:ring-blue-600"
+        className="bg-white dark:bg-neutral-800 p-2 rounded border border-gray-300 dark:border-neutral-700 text-gray-800 dark:text-white text-sm md:text-base lg:text-lg placeholder:text-gray-400 dark:placeholder:text-neutral-400 focus:outline-none focus:ring-2 focus:ring-blue-600"
         type="email"
         placeholder="e.g., contact@ankitmishra.pro"
       />
 
-      <label className="text-sm md:text-base lg:text-lg mt-3">
+      <label className="text-sm md:text-base lg:text-lg mt-3 text-gray-700 dark:text-white">
         Your message
       </label>
       <textarea
         name="message"
         autoComplete="off"
         required
-        className="bg-neutral-800 p-2 rounded border text-white border-neutral-700 text-sm md:text-base lg:text-lg placeholder:text-neutral-400 focus:outline-none focus:ring-2 focus:ring-blue-600"
+        className="bg-white dark:bg-neutral-800 p-2 rounded border border-gray-300 dark:border-neutral-700 text-gray-800 dark:text-white text-sm md:text-base lg:text-lg placeholder:text-gray-400 dark:placeholder:text-neutral-400 focus:outline-none focus:ring-2 focus:ring-blue-600"
         rows={3}
         placeholder="Type your message here..."
       />
