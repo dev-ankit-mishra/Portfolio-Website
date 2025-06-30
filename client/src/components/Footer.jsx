7dc7d5b2891be0c6a62be55e351b42e78912fe62
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-07-01 00:11:54 +0530

    added light mode background and text color

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,26 +2,41 @@ import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 export default function Footer() {
   return (
-    <footer className="w-full h-12 px-4   gap-6  tracking-wide  flex items-center justify-between  bg-neutral-950/90  text-neutral-400 border-t border-white/10 z-50 ">
+    <footer
+      className="w-full h-12 px-4 gap-6 tracking-wide flex items-center justify-between
+      bg-white/90 dark:bg-neutral-950/90 
+      text-gray-600 dark:text-neutral-400 
+      border-t border-gray-200 dark:border-white/10 z-50"
+    >
       <p className="text-xs md:text-sm">© 2025 Ankit Mishra Development.</p>
+
       <a
         href="mailto:contact@ankitmishra.pro"
-        className="text-sm hidden md:block hover:text-blue-400 transition-colors"
+        className="text-sm hidden md:block hover:text-blue-500 dark:hover:text-blue-400 transition-colors"
       >
         contact@ankitmishra.pro
       </a>
+
       <div className="flex justify-center items-center gap-4">
-        <a href="https://github.com/dev-ankit-mishra" target="_blank">
-          <FaGithub size={24} className="hover:text-gray-600 cursor-pointer" />
+        <a
+          href="https://github.com/dev-ankit-mishra"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaGithub
+            size={24}
+            className="hover:text-gray-800 dark:hover:text-gray-600 cursor-pointer transition-colors"
+          />
         </a>
 
         <a
           href="https://www.linkedin.com/in/ankit-mishra-44a179225/"
           target="_blank"
+          rel="noopener noreferrer"
         >
           <FaLinkedin
             size={24}
-            className="hover:text-gray-600 cursor-pointer"
+            className="hover:text-blue-700 dark:hover:text-blue-400 cursor-pointer transition-colors"
           />
         </a>
       </div>
