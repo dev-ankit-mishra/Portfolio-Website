e13cdf308c8d3e818616ba40a927342f9573474e
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-06-30 11:08:55 +0530

    made it responsive for xs screens

--- client/src/components/SkillsCards/Languages.jsx
+++ client/src/components/SkillsCards/Languages.jsx
@@ -14,37 +14,45 @@ import ToolTip from "../ToolTip";
 const languagesItems = [
   {
     id: 1,
-    icon: <SiC className="text-2xl xl:text-3xl  text-blue-500" />,
+    icon: <SiC className="text-xl sm:text-2xl xl:text-3xl  text-blue-500" />,
     label: "C",
     tooltip: "Low-level system programming language",
   },
   {
     id: 2,
-    icon: <FaJava className="text-2xl xl:text-3xl  text-red-600" />,
+    icon: <FaJava className="text-xl sm:text-2xl xl:text-3xl  text-red-600" />,
     label: "Java",
     tooltip: "Object-oriented programming for cross-platform apps",
   },
   {
     id: 3,
-    icon: <SiTypescript className="text-2xl xl:text-3xl  text-blue-400" />,
+    icon: (
+      <SiTypescript className="text-xl sm:text-2xl xl:text-3xl  text-blue-400" />
+    ),
     label: "TypeScript",
     tooltip: "Typed superset of JavaScript",
   },
   {
     id: 4,
-    icon: <SiJavascript className="text-2xl xl:text-3xl text-yellow-400" />,
+    icon: (
+      <SiJavascript className="text-xl sm:text-2xl xl:text-3xl text-yellow-400" />
+    ),
     label: "JavaScript",
     tooltip: "The language of the web",
   },
   {
     id: 5,
-    icon: <SiCplusplus className="text-2xl xl:text-3xl text-indigo-400" />,
+    icon: (
+      <SiCplusplus className="text-xl sm:text-2xl xl:text-3xl text-indigo-400" />
+    ),
     label: "C++",
     tooltip: "Powerful language for performance-critical applications",
   },
   {
     id: 6,
-    icon: <SiPython className="text-2xl xl:text-3xl text-yellow-300" />,
+    icon: (
+      <SiPython className="text-xl sm:text-2xl xl:text-3xl text-yellow-300" />
+    ),
     label: "Python",
     tooltip: "High-level language for AI, automation, and more",
   },
@@ -57,7 +65,7 @@ export const languages = languagesItems.map((lang) => {
       <ToolTip text={lang.tooltip}>
         <div className="flex flex-col items-center hover:scale-105 hover:shadow-md transition-all duration-200">
           {lang.icon}
-          <span className="xl:text-sm text-xs  tracking-wide mt-1">
+          <span className="xl:text-sm sm:text-xs text-[0.625rem]   tracking-wide mt-1">
             {lang.label}
           </span>
         </div>
