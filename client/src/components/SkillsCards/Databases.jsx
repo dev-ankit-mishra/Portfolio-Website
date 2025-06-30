8da2939200d40116b96f28bdf5d73008b4725be8
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-06-30 11:08:40 +0530

    made it responsive for xs screens

--- client/src/components/SkillsCards/Databases.jsx
+++ client/src/components/SkillsCards/Databases.jsx
@@ -7,25 +7,31 @@ import { SiMysql, SiMongodb, SiPostgresql, SiFirebase } from "react-icons/si";
 const DatabaseItems = [
   {
     id: 1,
-    icon: <SiMongodb className="text-2xl xl:text-3xl text-green-600" />,
+    icon: (
+      <SiMongodb className="text-xl sm:text-2xl xl:text-3xl text-green-600" />
+    ),
     label: "MongoDB",
     tooltip: "NoSQL database for flexible document storage",
   },
   {
     id: 2,
-    icon: <SiPostgresql className="text-2xl xl:text-3xl text-sky-600" />,
+    icon: (
+      <SiPostgresql className="text-xl sm:text-2xl xl:text-3xl text-sky-600" />
+    ),
     label: "PostgreSQL",
     tooltip: "Powerful open-source relational database",
   },
   {
     id: 3,
-    icon: <SiMysql className="text-2xl xl:text-3xl text-blue-400" />,
+    icon: <SiMysql className="text-xl sm:text-2xl xl:text-3xl text-blue-400" />,
     label: "MySQL",
     tooltip: "Popular relational database for web apps",
   },
   {
     id: 4,
-    icon: <SiFirebase className="text-2xl xl:text-3xl text-yellow-400" />,
+    icon: (
+      <SiFirebase className="text-xl sm:text-2xl xl:text-3xl text-yellow-400" />
+    ),
     label: "Firebase",
     tooltip: "Google's real-time database and hosting platform",
   },
@@ -38,7 +44,7 @@ export const databases = DatabaseItems.map((database) => {
       <ToolTip text={database.tooltip}>
         <div className="flex flex-col items-center hover:scale-105 hover:shadow-md transition-all duration-200">
           {database.icon}
-          <span className="xl:text-sm text-xs tracking-wide  mt-1">
+          <span className="xl:text-sm sm:text-xs text-[0.625rem]  tracking-wide  mt-1">
             {database.label}
           </span>
         </div>
