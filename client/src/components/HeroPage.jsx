f435c3c368be5fb9e642909d5a7eb6d68ed1cc7b
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-07-01 00:12:01 +0530

    added light mode background and text color

--- client/src/components/HeroPage.jsx
+++ client/src/components/HeroPage.jsx
@@ -10,13 +10,14 @@ export default function HeroPage() {
     <section id="Home" className="w-full h-screen font-Inter ">
       <NavBar />
 
-      <main className="w-full h-screen flex flex-col-reverse items-center  md:flex-row  md:justify-between  pt-32 pb-10 md:pb-0 md:pt-0 md:pr-20 md:gap-4  text-neutral-200 bg-gradient-to-br from-[#0f2027] via-[#203a43] to-[#2c5364]">
+      <main className="w-full h-screen flex flex-col-reverse items-center  md:flex-row  md:justify-between  pt-32 pb-10 md:pb-0 md:pt-0 md:pr-20 md:gap-4  text-neutral-200 bg-gradient-to-br dark:from-[#0f2027] dark:via-[#203a43] dark:to-[#2c5364] from-[#f8fafc] to-[#e2e8f0]">
         {/* Left Section: Name and Title */}
         <div className="px-4 h-full  flex flex-col gap-y-4 justify-center items-center ">
-          <h1 className="text-center tracking-wide text-2xl lg:text-3xl mt-14 font-bold">
+          <h1 className="text-center tracking-wide text-2xl lg:text-3xl mt-14 font-bold text-gray-800 dark:text-white">
             Hi I'm Ankit Mishra
           </h1>
-          <h2 className="text-center tracking-wide text-lg  lg:text-2xl font-semibold">
+
+          <h2 className="text-center tracking-wide text-lg lg:text-2xl font-semibold text-gray-700 dark:text-gray-300">
             <Typewriter
               words={[
                 "Full-Stack Developer",
@@ -31,14 +32,18 @@ export default function HeroPage() {
             />
           </h2>
 
-          <p className="lg:px-20 md:px-8 px-4 text-center text-sm lg:text-[1.08rem]  tracking-wide leading-relaxed  font-normal mb-6 mt-1">
+          <p className="lg:px-20 md:px-8 px-4 text-center text-sm lg:text-[1.08rem] tracking-wide leading-relaxed font-normal mb-6 mt-1 text-gray-600 dark:text-gray-300">
             I build interactive, scalable and responsive web apps using React,
             Next.js, and Node.js — from AI-powered tools to real-time
             collaboration platforms.
           </p>
+
           <div className="w-12 h-1 bg-blue-500 rounded-full mb-2"></div>
           <a
-            className="lg:py-[0.75rem] lg:px-10  px-6 py-2 flex justify-center items-center sm:text-sm md:text-base gap-2 lg:text-lg font-medium border border-cyan-500 text-cyan-500 hover:bg-cyan-500/10 hover:shadow-md hover:shadow-cyan-500/30 transition-all hover:scale-105 duration-150 rounded-lg mt-4 lg:mt-6 xl:mt-8 cursor-pointer"
+            className="lg:py-[0.75rem] lg:px-10  px-6 py-2 flex justify-center items-center sm:text-sm md:text-base gap-2 lg:text-lg font-medium
+    border border-cyan-500 text-cyan-600 dark:text-cyan-400 
+    hover:bg-cyan-500/10 hover:shadow-md hover:shadow-cyan-500/30
+    transition-all hover:scale-105 duration-150 rounded-lg mt-4 lg:mt-6 xl:mt-8 cursor-pointer"
             href={resume}
             target="_blank"
           >
