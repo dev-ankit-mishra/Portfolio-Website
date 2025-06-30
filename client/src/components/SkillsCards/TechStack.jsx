9fc279d18f32ff9434b62f4827e9fb9a6539739c
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-06-30 11:09:02 +0530

    made it responsive for xs screens

--- client/src/components/SkillsCards/TechStack.jsx
+++ client/src/components/SkillsCards/TechStack.jsx
@@ -14,37 +14,47 @@ import {
 const techStackItems = [
   {
     id: 1,
-    icon: <SiHtml5 className="text-2xl xl:text-3xl text-orange-500" />,
+    icon: (
+      <SiHtml5 className="text-xl sm:text-2xl xl:text-3xl text-orange-500" />
+    ),
     label: "HTML",
     tooltip: "Structure the content of web pages",
   },
   {
     id: 2,
-    icon: <SiTailwindcss className="text-2xl xl:text-3xl text-cyan-400" />,
+    icon: (
+      <SiTailwindcss className="text-xl sm:text-2xl xl:text-3xl text-cyan-400" />
+    ),
     label: "Tailwind",
     tooltip: "Utility-first CSS framework for rapid UI development",
   },
   {
     id: 3,
-    icon: <SiReact className="text-2xl xl:text-3xl text-sky-400" />,
+    icon: <SiReact className="text-xl sm:text-2xl xl:text-3xl text-sky-400" />,
     label: "React",
     tooltip: "Build dynamic user interfaces with components",
   },
   {
     id: 4,
-    icon: <SiNodedotjs className="text-2xl xl:text-3xl text-green-500" />,
+    icon: (
+      <SiNodedotjs className="text-xl sm:text-2xl xl:text-3xl text-green-500" />
+    ),
     label: "Node",
     tooltip: "JavaScript runtime for backend development",
   },
   {
     id: 5,
-    icon: <SiExpress className="text-2xl xl:text-3xl text-gray-300" />,
+    icon: (
+      <SiExpress className="text-xl sm:text-2xl xl:text-3xl text-gray-300" />
+    ),
     label: "Express",
     tooltip: "Minimalist Node.js framework for APIs",
   },
   {
     id: 6,
-    icon: <SiNextdotjs className="text-2xl xl:text-3xl text-white" />,
+    icon: (
+      <SiNextdotjs className="text-xl sm:text-2xl xl:text-3xl text-white" />
+    ),
     label: "Next",
     tooltip: "React framework for SSR and static websites",
   },
@@ -57,7 +67,7 @@ export const techStacks = techStackItems.map((lang) => {
       <ToolTip text={lang.tooltip}>
         <div className="flex flex-col items-center hover:scale-105 hover:shadow-md transition-all duration-200">
           {lang.icon}
-          <span className="xl:text-sm text-xs tracking-wide  mt-1">
+          <span className="xl:text-sm sm:text-xs text-[0.625rem] tracking-wide  mt-1">
             {lang.label}
           </span>
         </div>
