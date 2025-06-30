b3945935c62888b4855cf65327c98d5835a77904
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-07-01 00:12:07 +0530

    added light mode background and text color

--- client/src/components/MajorProject.jsx
+++ client/src/components/MajorProject.jsx
@@ -3,22 +3,29 @@ import Cards from "./MajorProjectCard/Cards";
 export default function Project() {
   return (
     <section
-      className=" w-full bg-gradient-to-br py-6 from-[#1e1b4b] via-[#312e81] to-[#1e1b4b]
-
-
-
-
-
-"
       id="Projects"
+      className="w-full py-6 
+                 bg-white dark:bg-gradient-to-br 
+                 dark:from-[#1e1b4b] dark:via-[#312e81] dark:to-[#1e1b4b]"
     >
       <header>
-        <h1 className="text-2xl md:text-3xl text-center font-bold pt-4 pb-2 text-neutral-100">
+        <h1
+          className="text-2xl md:text-3xl text-center font-bold pt-4 pb-2 
+                       text-gray-900 dark:text-neutral-100"
+        >
           Major Project
         </h1>
-        <div className="w-10 h-1 bg-blue-500 hover:w-24 transition-all duration-200 rounded-2xl mx-auto mt-2 mb-4" />
+        <div
+          className="w-10 h-1 bg-blue-600 hover:w-24 transition-all duration-200 
+                        rounded-2xl mx-auto mt-2 mb-4"
+        />
       </header>
-      <main className="w-full h-full  flex flex-col  gap-10  [@media(min-width:800px)]:flex-row justify-evenly px-4 pb-10 items-center py-4 ">
+
+      <main
+        className="w-full h-full flex flex-col gap-10 
+                      [@media(min-width:800px)]:flex-row 
+                      justify-evenly px-4 pb-10 items-center py-4"
+      >
         <Cards />
       </main>
     </section>
